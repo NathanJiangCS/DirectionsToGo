2cedfebaf483ab4350ee303c6b8614756367321b
Nathan Jiang <NathanJiangCS@users.noreply.github.com>
2017-05-24 14:44:27 -0500

    Update send_sms_twilio.py

--- send_sms_twilio.py
+++ send_sms_twilio.py
@@ -1,1 +1,14 @@
+from twilio.rest import TwilioRestClient
+from twilio import TwilioRestException
 
+def send_sms(msg, number):
+	try:
+		client = TwilioRestClient(account="AC42268174c5815f26cd5084406a48b7e5",
+							  token="f1a3d61fe159f9e6876070a33bf2d648")
+
+		client.messages.create(from_="(647) 691 3881",
+						  to=number,
+						  body=msg)
+
+	except TwilioRestException as e:
+		print e
